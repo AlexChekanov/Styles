62c5d3dbb4a8d93f129173b5d47cf9dd0f1ec89a
Alexey Chekanov <chekanov@hotmail.com>
2018-03-15 15:12:33 -0400

    Initial Commit

---
+++ Styles/Styles.h
@@ -0,0 +1,19 @@
+//
+//  Styles.h
+//  Styles
+//
+//  Created by Alexey Chekanov on 3/15/18.
+//  Copyright © 2018 Tips & Tricks, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Styles.
+FOUNDATION_EXPORT double StylesVersionNumber;
+
+//! Project version string for Styles.
+FOUNDATION_EXPORT const unsigned char StylesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Styles/PublicHeader.h>
+
+
